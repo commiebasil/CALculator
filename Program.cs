a17c69939967c946ded9df95a40f2570e657578d
pidoras12312431412 <fnaffun22816@gmail.com>
2021-10-27 22:59:57 +0300

    Update Program.cs
    Перевёл на Switch

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Globalization;
+using System.Globalization; 
 
 namespace lessons
 {
@@ -11,77 +11,82 @@ namespace lessons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Калкулятор v 0.0.2");
+            Console.WriteLine("Калкулятор v 0.0.3");
             Console.WriteLine("--------------");
             Console.WriteLine("Введите первое число");
             string str = Console.ReadLine();
             bool a1 = double.TryParse(str, out double a);
-            if (a1)
+            switch (a1)
             {
-                Console.WriteLine("Введите второе число");
-                str = Console.ReadLine();
-                bool b1 = double.TryParse(str, out double b);
-                if (b1)
-                {
-                    Console.WriteLine("ещё одно число? да/нет");
-                    string var = Console.ReadLine();
-                    if (var == "да")
+                case true:
+                    Console.WriteLine("Введите второе число");
+                    str = Console.ReadLine();
+                    bool b1 = double.TryParse(str, out double b);
+                    switch (b1)
                     {
-                        Console.WriteLine("Введите третье число");
-                        str = Console.ReadLine();
-                        bool c1 = double.TryParse(str, out double c);
-                        if (c1)
-                        {
-                            double[] array = new double[4];
-                            array[0] = a + b + c;
-                            array[1] = a - b - c;
-                            array[2] = a * b * c;
-                            array[3] = a / b / c;
-                            Console.WriteLine("Результаты:");
-                            Console.WriteLine("Сложение: " + array[0]);
-                            Console.WriteLine("Вычитание: " + array[1]);
-                            Console.WriteLine("Умножение: " + array[2]);
-                            Console.WriteLine("Деление: " + array[3]);
+                        case false:
+                            Console.WriteLine("Ошибка. Код 1. Разрешено вводить только числа.");
                             Console.ReadKey();
-                        }
-                        else
-                        {
-                            Console.WriteLine("Код ошибки 1. Разрешено вводить только цифры.");
-                            Console.ReadKey();
-                        }
-                    }
-                    if (var == "нет")
-                    {
-                        double[] array = new double[4];
-                        array[0] = a + b;
-                        array[1] = a - b;
-                        array[2] = a * b;
-                        array[3] = a / b;
-                        Console.WriteLine("Результаты:");
-                        Console.WriteLine("Сложение: " + array[0]);
-                        Console.WriteLine("Вычитание: " + array[1]);
-                        Console.WriteLine("Умножение: " + array[2]);
-                        Console.WriteLine("Деление: " + array[3]);
-                        Console.ReadKey();
+                            break;
+                        case true:
+                            Console.WriteLine("Ещё одно число? да/нет");
+                            string var = Console.ReadLine();
+                            switch (var)
+                            {
+                                case "да":
+                                    Console.WriteLine("Введите третье число");
+                                    str = Console.ReadLine();
+                                    bool c1 = double.TryParse(str, out double c);
+                                    switch (c1)
+                                    {
+                                        case false:
+                                            Console.WriteLine("Ошибка. Код 1. Разрешено вводить только числа.");
+                                            Console.ReadKey();
+                                            break;
+                                        case true:
+                                            double[] array1 = new double[4];
+                                            array1[0] = a + b + c;
+                                            array1[1] = a - b - c;
+                                            array1[2] = a * b * c;
+                                            array1[3] = a / b / c;
+                                            Console.WriteLine("Результаты:");
+                                            Console.WriteLine("Сложение: " + array1[0]);
+                                            Console.WriteLine("Вычитание: " + array1[1]);
+                                            Console.WriteLine("Умножение: " + array1[2]);
+                                            Console.WriteLine("Деление: " + array1[3]);
+                                            Console.ReadKey();
+                                            break;
 
+
+
+                                    }
+                                    break;
+                                case "нет":
+                                    double[] array = new double[4];
+                                    array[0] = a + b;
+                                    array[1] = a - b;
+                                    array[2] = a * b;
+                                    array[3] = a / b;
+                                    Console.WriteLine("Результаты:");
+                                    Console.WriteLine("Сложение: " + array[0]);
+                                    Console.WriteLine("Вычитание: " + array[1]);
+                                    Console.WriteLine("Умножение: " + array[2]);
+                                    Console.WriteLine("Деление: " + array[3]);
+                                    Console.ReadKey();
+                                    break;
+                                default:
+                                    Console.WriteLine("Ошибка. Код 2. Разрешено вводить только да/нет");
+                                    Console.ReadKey();
+                                    break;
+                            }
+                            break;
                     }
-                    else
-                    {
-                        Console.WriteLine("Код ошибки 2. Можно вводить только да/нет.");
-                        Console.ReadKey();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Код ошибки 1. Разрешено вводить только цифры.");
+                    break;
+                case false:
+                    Console.WriteLine("Ошибка. Код 1. Разрешено вводить только числа.");
                     Console.ReadKey();
-                }
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Код ошибки 1. Разрешено вводить только цифры.");
-                Console.ReadKey();
-            }
-        }
+        } 
     }
 }
